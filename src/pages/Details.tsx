8969ba8f533c7807a0bcd2c6bd907922953c8b9b
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-07-22 17:55:53 +0100

    feat: added details

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,4 +1,5 @@
 import data from "../../data/processed.json";
+import "../css/Details.css";
 import { useState, useEffect } from "react";
 
 const Details = ({ id }: { id: number }) => {  
@@ -39,9 +40,49 @@ const [LLMData, setLLMData] = useState<LLMDataType | undefined>();
   }, []);
 
   return (
-    <div>
-      {LLMData && LLMData.name}
-    </div>
+    <main>
+      <h1>
+        {LLMData && LLMData.name}
+        {!LLMData && "Invalid LLM"}
+      </h1>
+      {LLMData && (
+        <div className="container">
+          <div className="info-box">
+            {/* 
+            There is no "type" attribute in the LLMData.
+
+            <div className="item">
+              <p><label>Type: </label> {LLMData.type}</p>
+            </div> 
+            
+            */}
+            <div className="item">
+              <p><label className="field">Organization: </label> {LLMData.organization}</p>
+            </div>
+            <div className="item">
+              <p><label className="field">Date Created: </label> {LLMData.created_date}</p>
+            </div>
+            <div className="item">
+              <p><label className="field">Size: </label> {LLMData.size}</p>
+            </div>
+          </div>
+          <div className="info-box">
+            <div>
+              <label className="field">Modality: </label>
+              <p><label className="field">In: </label>{LLMData.input_modality}</p>
+              <p><label className="field">Out: </label>{LLMData.output_modality}</p>
+            </div>
+            <div className="item"><p><label className="field">Access: </label>{LLMData.access}</p></div>
+            <div className="item"><p><label className="field">License: </label>{LLMData.license}</p></div>
+          </div>
+          <div className="info-box">
+            <div className="item">
+              <p><label className="field">Dependencies: </label> {LLMData.dependencies !== "" ? LLMData.dependencies : "None"}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </main>
   );
 }
 
